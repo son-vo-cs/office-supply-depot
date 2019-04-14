4cab9e29a3cc39bdd675f5fb000f803c65d078bf
son-vo-cs <43371971+son-vo-cs@users.noreply.github.com>
2019-04-14 07:49:03 -0700

    Update ApiService.js

--- frontend/src/common/services/ApiService.js
+++ frontend/src/common/services/ApiService.js
@@ -2,13 +2,24 @@ class ApiService {
     constructor() {
 
     }
-
-    simpleApi() {
-        let url = 'https://api.github.com/userss/AlexHHsiao';
-        return fetch(url, {
-            method: 'GET'
-        }).then(resp => resp.json());
+    apiOSD(type, data)
+    {
+        URL = 'http://localhost:3006/' + type
+        return fetch(URL, {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: new Headers({'Content-Type': 'application/json'})
+        }).then(response => {
+            return response.json();
+        }).catch(error =>{
+            return JSON.stringify(error.message)
+        });
     }
+
+    login(data){return this.apiOSD('login',data)};
+    register(data){return this.apiOSD('register',data)};
+    getAll(){return this.apiOSD('getAll',{"a":2})};
+    getItem(data) {return this.apiOSD('getItem',data)};
 }
 
 export default new ApiService();
